fc9f4caa28643da14689275c4f7e4922bae6b5fb
Will Lewis <lumitedubbz@gmail.com>
2019-11-02 22:25:07 +0000

    New automatic help responses.

--- src/Common/Configuration.cs
+++ src/Common/Configuration.cs
@@ -21,13 +21,25 @@ namespace NukoBot.Common
             new HelpResponse()
             {
                 TriggerWords = { "How do I install the mod", "How do I install mods", "How to install mods", "How do I get the mod", "How to get the mod", "Where do I get the mod" },
-                Response = "To download the mod: Download the latest ZIP file from `#download`.\n\nTo install the mod:\n1) Extract the ZIP file.\n2) Open a new file explorer window.\n3)Go to `C:\\Program Files (x86)\\Steam\\steamapps\\common\\Bloons TD Battles`\n4) Copy the `Assets` and `ExtraAssets` folders from the mod folder and paste it into the BTD Battles folder.\n5) Overwrite any files it asks you to."
+                Response = "**You might want to use another Steam account to prevent your main from being banned!**\n\nTo download the mod: Download the latest ZIP file from `#download`.\n\nTo install the mod:\n1) Extract the ZIP file.\n2) Open a new file explorer window.\n3)Go to `C:\\Program Files (x86)\\Steam\\steamapps\\common\\Bloons TD Battles`\n4) Copy the `Assets` and `ExtraAssets` folders from the mod folder and paste it into the BTD Battles folder.\n5) Overwrite any files it asks you to."
             },
 
             new HelpResponse()
             {
                 TriggerWords = { "How do points work", "How do I get points", "How do I submit screenshots", "Where do I get points", "Where do I submit screenshots", "What are points" },
                 Response = "You may submit screenshots in #submit-screenshots using the command `>submit` with your screenshot attached.\n\nThe administrators will look at your screenshot and award you points based on 3 factors:\nThe round you died on,\nThe difficulty of the map you played on and\nIf you played with someone else in the server.\n\nIf you played on the easy map, you get 75% of the round you got in points. On the normal map you get 100% of the round you got in points. On the hard map, you get 125% of the round you got in points.\n\nIf you played with someone else in the server and they also submit a screenshot, you both get 10% bonus points."
+            },
+
+            new HelpResponse()
+            {
+                TriggerWords = { "What upgrades are there", "What upgrades will there be", "What upgrades can we get", "Where can I see the upgrades", "What are upgrades", "What are the upgrades" },
+                Response = "You can view the available upgrades from this spreadsheet: https://docs.google.com/spreadsheets/d/1v-MLviIsb9qTvKPoLmsrk7lfxnM8vkC2r6lKVjE_ObQ/edit - remember to check out the other tabs to view the different types of upgrade."
+            },
+
+            new HelpResponse()
+            {
+                TriggerWords = { "What towers are there", "What enemies are there" },
+                Response = "You can view the towers and enemies in these documents:\n\nTowers: https://docs.google.com/document/d/1b5doat4o_88qCAkb-wXcW1VWqMzjXodTMT1PI9E6NKY/edit\nEnemies:https://docs.google.com/document/d/1ZeV_ZCo-cLXE0zp1zkyI1epP4aciqKADz9T57bXwOwE/edit"
             }
         };
 
