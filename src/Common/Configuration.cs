43927475b301c3adbd8baa82a447caa7949b454d
Will Lewis <lumitedubbz@gmail.com>
2019-11-04 21:32:41 +0000

    More detail on help response - 1.1.12

--- src/Common/Configuration.cs
+++ src/Common/Configuration.cs
@@ -8,7 +8,7 @@ namespace NukoBot.Common
     {
         public static readonly string Prefix = ">";
 
-        public static readonly string Version = "1.1.11";
+        public static readonly string Version = "1.1.12";
 
         public static readonly string Game = Prefix + "help";
 
@@ -26,8 +26,8 @@ namespace NukoBot.Common
 
             new HelpResponse()
             {
-                TriggerWords = { "How do points work", "How do I get points", "How do I submit screenshots", "Where do I get points", "Where do I submit screenshots", "What are points", "How exactly does the point system award points " },
-                Response = "You may submit screenshots in #submit-screenshots using the command `>submit` with your screenshot attached.\n\nThe administrators will look at your screenshot and award you points based on 3 factors:\nThe round you died on,\nThe difficulty of the map you played on and\nIf you played with someone else in the server.\n\nIf you played on the easy map, you get 75% of the round you got in points. On the normal map you get 100% of the round you got in points. On the hard map, you get 125% of the round you got in points.\n\nIf you played with someone else in the server and they also submit a screenshot, you both get 10% bonus points."
+                TriggerWords = { "How do points work", "How do I get points", "How do I submit screenshots", "Where do I get points", "Where do I submit screenshots", "What are points", "How exactly does the point system award points", "What do points do", "What can I spend points on", "How do I use points" },
+                Response = "You may submit screenshots in #submit-screenshots using the command `>submit` with your screenshot attached.\n\nThe administrators will look at your screenshot and award you points based on 3 factors:\nThe round you died on,\nThe difficulty of the map you played on and\nIf you played with someone else in the server.\n\nIf you played on the easy map, you get 75% of the round you got in points. On the normal map you get 100% of the round you got in points. On the hard map, you get 125% of the round you got in points.\n\nIf you played with someone else in the server and they also submit a screenshot, you both get 10% bonus points.\n\nThe server's total point count (which you can view by using `>points`) can be used to upgrade certain things. To view these, say \"what upgrades are there\"."
             },
 
             new HelpResponse()
