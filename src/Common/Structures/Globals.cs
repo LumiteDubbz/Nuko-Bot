6908b96fca4cffdde8cc5b400cb62ae17ce13cc1
Will Lewis <lumitedubbz@gmail.com>
2019-12-10 22:38:50 +0000

    Eval stuff

---
+++ src/Common/Structures/Globals.cs
@@ -0,0 +1,25 @@
+﻿using Discord;
+using MongoDB.Driver;
+using NukoBot.Services;
+
+namespace NukoBot.Common.Structures
+{
+    public class Globals
+    {
+        public Globals(IDiscordClient client, IGuild guild, IMongoDatabase database, Text text)
+        {
+            Client = client;
+            Guild = guild;
+            Database = database;
+            Text = text;
+        }
+
+        public IDiscordClient Client { get; }
+
+        public IGuild Guild { get; }
+
+        public IMongoDatabase Database { get; }
+
+        public Text Text { get; }
+    }
+}
